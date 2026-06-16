6386f9a89beffb3237255f2b8dee928a4bca4f42
Priyanshu Sharma <ps200410@gmail.com>
2026-06-16 11:42:42 +0530

    first commit

---
+++ social/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+DEBUG=os.environ.get('DEBUG')
+SECRET_KEY=os.environ.get('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
+    }
+}
